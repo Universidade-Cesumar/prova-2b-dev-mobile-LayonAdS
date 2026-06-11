ab0000781a484df477065d3d13b5a7069aa57ee6
LayonAdS <locosilia5@gmail.com>
2026-06-11 20:31:37 -0300

    fazendo set para armazenar as informações e fazendo a função para carregar os materiais

--- sysalmoxarifado/App.js
+++ sysalmoxarifado/App.js
@@ -4,7 +4,45 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, Activity
 // importação das bilbiotecas necessárias para o funcionamento do aplicativo
 
 export default function App() {
-  return (
+
+const [nome, setNome] = useState('');  // Armazena o nome digitado no formulário
+const [quantidade, setQuantidade] = useState(''); // Armazena a quantidade digitada
+const [materiais, setMateriais] = useState([]); // Armazena a lista de materiais vindos da API
+const [loading, setLoading] = useState(false); // Controla o indicador de carregamento
+
+// Endereço da MockAPI.
+// Todas as requisições GET e POST serão feitas para esta URL.
+const API_URL ='https://6a18c28d23c3626470abfea4.mockapi.io/api/v1/Materiais';
+
+async function carregarMateriais() {
+// Busca todos os materiais cadastrados na MockAPI.
+  try {
+
+    // Exibe indicador de carregamento
+    setLoading(true);
+
+    // Faz a requisição GET
+    const response = await fetch(API_URL);
+
+    // Converte a resposta para JSON
+    const data = await response.json();
+
+    // Salva os materiais no estado
+    setMateriais(data);
+
+  } catch (error) {
+
+    // Exibe erro no console
+    console.log(error);
+
+  } finally {
+
+    // Esconde o indicador de carregamento
+    setLoading(false);
+  }
+}
+
+return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />

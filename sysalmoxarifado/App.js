61c86e7544c98841257b1af83e9e52c4cdb22db9
LayonAdS <locosilia5@gmail.com>
2026-06-11 20:09:54 -0300

    importando as bibliotecas para o funcionamento do app

--- sysalmoxarifado/App.js
+++ sysalmoxarifado/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+// importação das bilbiotecas necessárias para o funcionamento do aplicativo
 
 export default function App() {
   return (
